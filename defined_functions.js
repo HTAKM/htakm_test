b4dc143ab6eb6888f6b0e02b1afc68a9c1d7ff8c
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2024-06-16 17:26:09 +0800

    :)

--- defined_functions.js
+++ defined_functions.js
@@ -1,5 +1,5 @@
 function add_rows(){
-    var number_of_rows = document.getElemensById('courses_given.course_rows_given').length+1;
+    var number_of_rows = document.getElementById('courses_given.course_rows_given').length+1;
     document.getElementById('courses_given').innerHTML += '<div class="course_rows_given">' +
         '   <div class="form_course">' + 
         '       <input type="text" id="inputCourse'+number_of_rows+'">' + 
