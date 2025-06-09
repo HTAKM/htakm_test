5a4d03938fac554545f1d3d923e18e77564e4862
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2025-06-09 23:43:42 +0800

    Move grades into csv file and display it using js

--- js/grade/grade.js
+++ js/grade/grade.js
@@ -1,27 +1,44 @@
 function loadGrade() {
-    let content;
     fetch("js/grade/course-and-grade.csv")
         .then((res) => res.text())
         .then((text) => {
-            content = $.csv.toObjects(text);
+            let content = $.csv.toObjects(text);
+            const GPAValidGrade = ['A+','A','A-','B+','B','B-','C+','C','C-','D','F'];
+            const gradeTable = $("#grade-table");
+            let gradeRow, termCol, codeCol, nameCol, creditCol, gradeCol;
+            let term = "", termCount = 0;
+            for (let i = 0; i < content.length; ++i) {
+                if (!(GPAValidGrade.includes(content[i]["Grade"]))) continue;
+                gradeRow = $('<tr>');
+                if (["Year",content[i]["Year"],content[i]["Term"]].join(" ") == term) {
+                    ++termCount;
+                } else {
+                    $('.course-term:contains('+term+')').attr('rowspan', termCount);
+                    term = ["Year",content[i]["Year"],content[i]["Term"]].join(" ");
+                    termCount = 1;
+                    termCol = $('<td>', {class: "course-term"}).html(term);
+                    gradeRow.append(termCol);
+                }
+                codeCol = $('<td>', {class: "course-code"}).html([content[i]["Prefix"],content[i]["Code"]].join(" "));
+                nameCol = $('<td>', {class: "course-name"}).html(content[i]["Name"]);
+                creditCol = $('<td>', {class: "course-credit"}).html(content[i]["Credit"]);
+                gradeCol = $('<td>', {class: "course-grade"}).html(content[i]["Grade"]);
+                gradeRow.append(codeCol);
+                gradeRow.append(nameCol);
+                gradeRow.append(creditCol);
+                gradeRow.append(gradeCol);
+                gradeTable.append(gradeRow);
+            }
+            $('.course-term:contains('+term+')').attr('rowspan', termCount);
         })
         .catch((e) => console.error(e));
-    console.log(JSON.stringify(content, undefined, 2));
-    const gradeTable = $("#grade-table");
-    let gradeRow = $('<tr>');
-    let termCol = $('<td>', {class: "course-term"});
-    let codeCol = $('<td>', {class: "course-code"});
-    let nameCol = $('<td>', {class: "course-name"});
-    let creditCol = $('<td>', {class: "course-credit"});
-    let gradeCol = $('<td>', {class: "course-grade"});
-    gradeRow.append(termCol);
-    gradeRow.append(codeCol);
-    gradeRow.append(nameCol);
-    gradeRow.append(creditCol);
-    gradeRow.append(gradeCol);
-    gradeTable.append(gradeRow);
+}
+
+function modifyTable() {
+
 }
 
 $(function() {
     loadGrade();
+    modifyTable();
 });
