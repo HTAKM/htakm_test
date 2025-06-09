f4c5a8a426271cc600ec5b4d867787dbf510a48b
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2025-06-10 00:36:04 +0800

    Modify table behavior so that element with rowspan can be highlighted

--- js/grade/grade.js
+++ js/grade/grade.js
@@ -1,5 +1,5 @@
-function loadGrade() {
-    fetch("js/grade/course-and-grade.csv")
+async function loadGrade() {
+    await fetch("js/grade/course-and-grade.csv")
         .then((res) => res.text())
         .then((text) => {
             let content = $.csv.toObjects(text);
@@ -11,18 +11,19 @@ function loadGrade() {
                 if (!(GPAValidGrade.includes(content[i]["Grade"]))) continue;
                 gradeRow = $('<tr>');
                 if (["Year",content[i]["Year"],content[i]["Term"]].join(" ") == term) {
+                    termCol = $('<td>', {class: "course-term", style: "display:none"}).html(term);
                     ++termCount;
                 } else {
-                    $('.course-term:contains('+term+')').attr('rowspan', termCount);
+                    $('.course-term:contains('+term+')').first().attr('rowspan', termCount);
                     term = ["Year",content[i]["Year"],content[i]["Term"]].join(" ");
                     termCount = 1;
                     termCol = $('<td>', {class: "course-term"}).html(term);
-                    gradeRow.append(termCol);
                 }
                 codeCol = $('<td>', {class: "course-code"}).html([content[i]["Prefix"],content[i]["Code"]].join(" "));
                 nameCol = $('<td>', {class: "course-name"}).html(content[i]["Name"]);
                 creditCol = $('<td>', {class: "course-credit"}).html(content[i]["Credit"]);
                 gradeCol = $('<td>', {class: "course-grade"}).html(content[i]["Grade"]);
+                gradeRow.append(termCol);
                 gradeRow.append(codeCol);
                 gradeRow.append(nameCol);
                 gradeRow.append(creditCol);
@@ -34,11 +35,12 @@ function loadGrade() {
         .catch((e) => console.error(e));
 }
 
-function modifyTable() {
-
-}
-
-$(function() {
-    loadGrade();
-    modifyTable();
+$(async function() {
+    await loadGrade();
+    $('.custom-table tbody tr').on("mouseenter", (event) => 
+        $(this).find('.course-term:contains('+$(event.target).parent().find(".course-term").html()+')').first().attr("style", "background-color: #d4a5eb;")
+    )
+    $('.custom-table tbody tr').on("mouseleave", (event) => 
+        $(this).find('.course-term:contains('+$(event.target).parent().find(".course-term").html()+')').first().attr("style", "")
+    )
 });
